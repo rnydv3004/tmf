105491300580f5ac44f34436a9f3c8032b379627
Aryan <yaryan3087@gmail.com>
2023-09-19 20:53:42 +0530

    add

--- src/app/api/mailer/route.ts
+++ src/app/api/mailer/route.ts
@@ -2,49 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 var nodemailer = require("nodemailer");
 
-function sendMail(subject: any, toEmail: any, otpText: any) {
-
-    console.log("Hi this is a mail")
-
-    // var transporter = nodemailer.createTransport({
-    //     service: "gmail",
-    //     auth: {
-    //         user: process.env.NODEMAILER_EMAIL,
-    //         pass: process.env.NODEMAILER_PW,
-    //     },
-    // });
-
-    // var mailOptions = {
-    //     from: process.env.NODEMAILER_EMAIL,
-    //     to: toEmail,
-    //     subject: "Taxmechanic Appointment Confirmation",
-    //     text: otpText,
-    // };
-
-    // transporter.sendMail(mailOptions, function (error: string | undefined, info: any) {
-    //     if (error) {
-    //         console.log('Error occured while sending mail:', error)
-    //         throw new Error(error);
-    //     } else {
-    //         console.log("Email Sent to ",toEmail);
-    //         return true;
-    //     }
-    // });
-
-    return NextResponse.json({
-        message: "Email sent!",
-        status: 200
-    }, { status: 200 });
-}
-
-
 export async function POST(request: NextRequest) {
     try {
 
         const reqBody = await request.json()
         const { firstName, email, date, time } = reqBody
-        console.log("Sending mail to ",email)
-        const otpText = `Dear Mr./Ms. ${firstName},
+
+        console.log("Sending mail to ", email)
+        console.log(`First Name: ${firstName},Email: ${email},Date: ${date},Time: ${time}`)
+
+        const otpText = `Hey ${firstName}
+
 
 We would like to inform you that a complimentary consultation appointment has been scheduled with one of our tax professionals at ${date} at ${time}. The meeting will take place online, and you can access the meeting link provided below. We appreciate your appointment booking.
         
@@ -60,7 +28,37 @@ Thank you for choosing Taxmechanic for your tax-related needs.
 Sincerely,
 The Taxmechanic Team`
 
-        sendMail("Taxmechanic Appointment Confirmation", email, otpText)
+        console.log("Hi this is a mail")
+
+        var transporter = await nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.NODEMAILER_EMAIL,
+                pass: process.env.NODEMAILER_PW,
+            },
+        });
+
+        var mailOptions = {
+            from: process.env.NODEMAILER_EMAIL,
+            to: email,
+            subject: "Taxmechanic Appointment Confirmation",
+            text: otpText,
+        };
+
+        console.log("Mailoption set successfully!")
+
+        const sendStatus = await transporter.sendMail(mailOptions);
+
+        if (sendStatus.accepted.length > 0) {
+            console.log("Email Sent to ", email);
+            return NextResponse.json({
+                message: "Email sent!",
+                status: 200,
+            });
+        } else {
+            console.error("Email not accepted for delivery.");
+            throw new Error("Email not accepted for delivery.");
+        }
 
     } catch (error: any) {
         return NextResponse.json({ error: error.message });
