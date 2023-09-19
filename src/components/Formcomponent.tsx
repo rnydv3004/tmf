ef46455661de07b66a69ab1bbb3bd4fbb54e0a49
Aryan Yadav <123151923+rnydv3004@users.noreply.github.com>
2023-09-19 13:19:14 +0530

    Update Formcomponent.tsx

--- src/components/Formcomponent.tsx
+++ src/components/Formcomponent.tsx
@@ -82,6 +82,7 @@ export default function Formcomponent() {
 
     function chips(text: string, state: boolean) {
         return (<button
+            key={text} 
             type='button'
             disabled={state}
             className={`disabled:bg-slate-500 ${selectedSlot === text ? 'bg-green-600' : 'bg-blue-500'} h-fit w-fit py-1 px-5 rounded-full text-sm transition-transform transform ease-in-out duration-300 hover:scale-105 active:scale-100 select-none cursor-pointer text-white`}
