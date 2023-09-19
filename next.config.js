3844ced9421990476cc0e0f01c9460cf6c8a8f6a
Aryan <yaryan3087@gmail.com>
2023-09-19 16:46:06 +0530

    adding CORS configurations

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
 module.exports = nextConfig;
 
@@ -7,6 +7,27 @@ module.exports = {
   env: {
     NODEMAILER_EMAIL: "taryan3087@gmail.com",
     NODEMAILER_PW: "iouz mgiw krir fruh",
-    DB_URL: "https://appointments-b9eae-default-rtdb.firebaseio.com/"
+    DB_URL: "https://appointments-b9eae-default-rtdb.firebaseio.com/",
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
