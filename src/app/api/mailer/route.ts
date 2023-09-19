e58fa10be4b6d2ef6879c5002f22a3831f1b1a64
Aryan <yaryan3087@gmail.com>
2023-09-19 18:40:46 +0530

    mail changes

--- src/app/api/mailer/route.ts
+++ src/app/api/mailer/route.ts
@@ -4,30 +4,32 @@ var nodemailer = require("nodemailer");
 
 function sendMail(subject: any, toEmail: any, otpText: any) {
 
-    var transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.NODEMAILER_EMAIL,
-            pass: process.env.NODEMAILER_PW,
-        },
-    });
+    console.log("Hi this is a mail")
 
-    var mailOptions = {
-        from: process.env.NODEMAILER_EMAIL,
-        to: toEmail,
-        subject: "Taxmechanic Appointment Confirmation",
-        text: otpText,
-    };
+    // var transporter = nodemailer.createTransport({
+    //     service: "gmail",
+    //     auth: {
+    //         user: process.env.NODEMAILER_EMAIL,
+    //         pass: process.env.NODEMAILER_PW,
+    //     },
+    // });
 
-    transporter.sendMail(mailOptions, function (error: string | undefined, info: any) {
-        if (error) {
-            console.log('Error occured while sending mail:', error)
-            throw new Error(error);
-        } else {
-            console.log("Email Sent to ",toEmail);
-            return true;
-        }
-    });
+    // var mailOptions = {
+    //     from: process.env.NODEMAILER_EMAIL,
+    //     to: toEmail,
+    //     subject: "Taxmechanic Appointment Confirmation",
+    //     text: otpText,
+    // };
+
+    // transporter.sendMail(mailOptions, function (error: string | undefined, info: any) {
+    //     if (error) {
+    //         console.log('Error occured while sending mail:', error)
+    //         throw new Error(error);
+    //     } else {
+    //         console.log("Email Sent to ",toEmail);
+    //         return true;
+    //     }
+    // });
 }
 
 
@@ -35,9 +37,9 @@ export async function POST(request: NextRequest) {
     try {
 
         const reqBody = await request.json()
-        const { lastName, email, date, time } = reqBody
-
-        const otpText = `Dear Mr./Ms. ${lastName},
+        const { firstName, email, date, time } = reqBody
+        console.log("Sending mail to ",email)
+        const otpText = `Dear Mr./Ms. ${firstName},
 
 We would like to inform you that a complimentary consultation appointment has been scheduled with one of our tax professionals at ${date} at ${time}. The meeting will take place online, and you can access the meeting link provided below. We appreciate your appointment booking.
         
