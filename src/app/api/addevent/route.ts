4a90da54a9a411cc9b9b94c0d85e676fbd8a550b
Aryan Yadav <123151923+rnydv3004@users.noreply.github.com>
2023-09-24 11:26:03 +0530

    Update route.ts

--- src/app/api/addevent/route.ts
+++ src/app/api/addevent/route.ts
@@ -62,7 +62,7 @@ export async function POST(request: NextRequest) {
         };
 
         const auth = new google.auth.GoogleAuth({
-            keyFile: 'avid-day-281003-fa0ab3903368.json',
+            keyFile: 'https://raw.githubusercontent.com/rnydv3004/tmf/master/avid-day-281003-fa0ab3903368.json',
             scopes: 'https://www.googleapis.com/auth/calendar', //full access to edit calendar
         });
 
